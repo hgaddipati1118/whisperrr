77d9a5d35c029742fb5c61f3b9551ad79ff239ae
hgaddipati1118 <hgaddipati1118@gmail.com>
2023-06-25 14:06:17 -0500

    Tweak question appearance on mobile a lil :)

--- components/McOption.js
+++ components/McOption.js
@@ -4,7 +4,7 @@ const ALPHABET = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
 'Y', 'Z' ];
 const ALPHABET2 = ['a', 'b', 'c','d','e','f','g','h','i','j','k','l','m','n','o','p',
 'q','r','s','t','u','v','w','x','y','z'];
-export default function McOption({option, index, value, setValue}){
+export default function McOption({option, index, value, setValue, clearHove}){
 
     function updateValue(val){
         if(value == val){
@@ -14,7 +14,7 @@ export default function McOption({option, index, value, setValue}){
         }
     }
     return(
-        <div className= {`flex ${value == option.choiceVal?"bg-cyan-800":"bg-cyan-400"} hover:bg-cyan-800 mb-5 
+        <div className= {`flex bg-cyan-400 ${value == option.choiceVal?"md:bg-cyan-800":" "} md:hover:bg-cyan-800 mb-5 
         border-orange-500 border-2 rounded-lg max-w-2/3 w-fit`}
         onClick = {() => updateValue(option.choiceVal)}>
         <div className = "my-4 ml-4 px-2 border-2 border-black text-lg h-8 text-center bg-cyan-200">
